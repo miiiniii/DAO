ea0064caa9e66a1be9b973e6f44e3b6490372f50
anseongchan <anseongchan@DESKTOP-N9D2JA2>
2022-02-24 18:52:20 +0900

    project import

---
+++ virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/VirtualAssetWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.virtualAsset.webServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualAssetWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualAssetWebServerApplication.class, args);
+	}
+
+}
---
+++ virtualAssetWebServer/src/test/java/com/virtualAsset/webServer/VirtualAssetWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtualAsset.webServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualAssetWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
