93cb8bc83ec54c236368853565abc0513485ba3e
tjdcks098 <44407212+tjdcks098@users.noreply.github.com>
2022-03-23 19:24:47 +0900

    --탐색탭 검색결과 리스트 레이아웃(완)
       -->탐색탭 기본 클럽 노출 데이터 받기&데이터 기반 동적 html컴포넌트 구현(완)

--- virtualAssetWebServer/frontend/src/App.jsx
+++ virtualAssetWebServer/frontend/src/App.jsx
@@ -4,8 +4,9 @@ import React, {useEffect, useState} from 'react';
 import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import MainIcon from "./pages/mainIcon.js";
-import Home from "./pages/home.js";
+import MainIcon from "./components/mainIcon.jsx";
+import Home from "./components/home.jsx";
+import Explore from "./components/explore.jsx";
 
 import customAxios from './scripts/customAxios';
 
@@ -15,35 +16,27 @@ import customAxios from './scripts/customAxios';
 
 
 function App() {
-  /*const [ip, setIp]=useState('');
+  const [auth,setAuth] =useState();
 
-  function callback(data){
-    setIp(data);
-  }
-  useEffect(
-    ()=>{
-      customAxios('/ip',callback);
-    },[]
-    );
-    
-    const [msg, setMsg]= useState([]);
-    function msgf(data){
-      setMsg(data);
-    }
-    useEffect(()=>{
-      customAxios('/hello', msgf);
-    },[]);
-    
-    */
-
-//현 스와이퍼 state
+  //현 스와이퍼 state
   let [swiper,setSwiper]=useState();
+
+  //로그인 검사
+  useEffect(
+    () => {
+      customAxios('/auth', (data) => {
+        setAuth(data);
+      });
+    }, []
+  );
+
+
   //현 아이콘 상태 state
   let [iconState, setIconState]=useState([true, false, false, false]);
   //아이콘 상태 변경 메소드
   function changeIcon(idx){
     let temp=[false, false, false, false];
-    temp[idx-1]=true;
+    temp[idx]=true;
     setIconState(temp);
   }
 
@@ -52,23 +45,24 @@ function App() {
       <header className="App-header">
           <Swiper 
             id='mainWindowContainer'
+
             //한페이지에 슬라이드하나
             slidesPerView={1}
             //슬라이드 사이 간격
             spaceBetween={20}
             //끝단으로 가면 반대편으로 슬와이프 가능
             loop={true}
-            //스와이퍼 init시 개체를 state에 저장
-            onSwiper={(sw)=>(setSwiper(sw))}
             //스와이프시 이벤트로 아이콘 상태 변경
-            //최초실행시 onslidechange가 먼저 발생해 swiper가 undefined 오류 발생. 예외처리
-            onSlideChange={()=>(swiper!=undefined?changeIcon((swiper.activeIndex+3)%4+1):console.log('swiper is undefined.'))}
+            onSlideChange={(sw)=>(
+              setSwiper(sw),
+              changeIcon((sw.realIndex))
+              )}
             >
             <SwiperSlide id="homeWindow">
-              <Home className='mainWindow'></Home>
+              <Home className='mainWindow' auth={auth} sw={swiper}/>
             </SwiperSlide>
             <SwiperSlide id="exploreWindow">
-              <div className='mainWindow'></div>
+              <Explore className='mainWindow' auth={auth} sw={swiper}></Explore>
             </SwiperSlide>
             <SwiperSlide id="assetWindow">
               <div className='mainWindow'></div>
@@ -78,17 +72,15 @@ function App() {
             </SwiperSlide>
           </Swiper>
         <div className='mainIconContainer'>
-          <MainIcon type='home' hl={iconState[0]} />
-          <MainIcon type='explore' hl={iconState[1]}/>
-          <MainIcon type='asset' hl={iconState[2]}/>
-          <MainIcon type='user' hl={iconState[3]}/>
+          <MainIcon type='home' hl={iconState[0]} sw={swiper} index={0}/>
+          <MainIcon type='explore' hl={iconState[1]} sw={swiper} index={1}/>
+          <MainIcon type='asset' hl={iconState[2]} sw={swiper} index={2}/>
+          <MainIcon type='user' hl={iconState[3]} sw={swiper} index={3}/>
         </div>
       </header>
     </div>
   );
 }
-
-
 
 
 export default App;
