87c04a2e53f7d38569c24592774688138ce0f68a
tjdcks098 <44407212+tjdcks098@users.noreply.github.com>
2022-03-30 18:55:58 +0900

    클럽배너 정보 클래스화, json화 메소드

--- virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/controller/TestController.java
+++ virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/controller/TestController.java
@@ -2,11 +2,14 @@ package com.virtualAsset.webServer.controller;
 
 
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.virtualAsset.webServer.data.Auth;
+import com.virtualAsset.webServer.data.ClubBanner;
 
 @RestController
 @RequestMapping("/api")
@@ -21,113 +25,25 @@ public class TestController {
 	private static ObjectMapper mapper=new ObjectMapper();
 	
 	@PostMapping("/publicClub")
-	public String pubClub(HttpServletRequest request) {
+	public String pubClub(HttpServletRequest request) throws JSONException, ParseException {
 		//test value
-				JSONArray ja=new JSONArray();
-				JSONObject jo=new JSONObject();
-				JSONObject jo1=new JSONObject();
-				JSONObject jo2=new JSONObject();
-				JSONObject jo3=new JSONObject();
-				try {
-					jo.put("name", "클럽 이름");
-					jo.put("concludedContract", "26");
-					jo.put("totalContract", "180");
-					jo.put("member", "111");
-					jo.put("recentActivities", "1개월");
-					jo.put("tag", new JSONArray().put("품목").put("품목").put("품목").put("품목"));
-					jo.put("intro", "클럽 소개글\n클럽 소개글");
-					ja.put(jo);
-					ja.put(jo);
-					jo1.put("name", "휴양지 공동 개발");
-					jo1.put("concludedContract", "15");
-					jo1.put("totalContract", "22");
-					jo1.put("member", "411");
-					jo1.put("recentActivities", "2주");
-					jo1.put("tag", new JSONArray().put("휴양지").put("부동산").put("저점"));
-					jo1.put("intro", "코로나로 저점이지만 앞으로 전망이 밝은 각종 휴양지를 전문으로 투자하실분들 모십니다.");
-					ja.put(jo1);			
-					jo2.put("name", "홍대 갤러리");
-					jo2.put("concludedContract", "46");
-					jo2.put("totalContract", "2130");
-					jo2.put("member", "11.5k");
-					jo2.put("recentActivities", "48분");
-					jo2.put("tag", new JSONArray().put("그림").put("조각"));
-					jo2.put("intro", "홍대 작가 유망주들의 작품 지분 투자");
-					ja.put(jo2);			
-					jo3.put("name", "동학 개미 운동");
-					jo3.put("concludedContract", "71");
-					jo3.put("totalContract", "182");
-					jo3.put("member", "85.4k");
-					jo3.put("recentActivities", "11일");
-					jo3.put("tag", new JSONArray().put("국내주식").put("해외주식").put("세력"));
-					jo3.put("intro", "소자본으로 항상 세력들에게 끌려다니는 개미들도 뭉치면 강하다!");
-					ja.put(jo3);
-					ja.put(jo);
-					ja.put(jo);
-					ja.put(jo);
-					ja.put(jo);
-					ja.put(jo);
-					ja.put(jo);					
-					ja.put(jo);
-					ja.put(jo);
-					ja.put(jo);
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-				
-				//return "";
-				return ja.toString();
+		ClubBanner standard =new ClubBanner("클럽이름", 26, 180, 111, "2022/03/30 16:22:45", "클럽 소개글\n클럽 소개글", new ArrayList<String>(Arrays.asList("품목","품목","품목","품목")));
+		ClubBanner example1=new ClubBanner("휴양지 공동 개발",15,22,411,"2021/03/29 20:11:02","코로나로 저점이지만 앞으로 전망이 밝은 각종 휴양지를 전문으로 투자하실분들 모십니다.", new ArrayList<String>(Arrays.asList("휴양지","부동산","저점")));
+		ClubBanner example2=new ClubBanner("홍대 갤러리",46,2130,11512,"2022/03/30 9:42:16","홍대 작가 유망주들의 작품 지분 투자", new ArrayList<String>(Arrays.asList("그림","조각")));
+		ClubBanner example3=new ClubBanner("동학 개미 운동",71,182,85429,"2022/02/15 20:11:02","소자본으로 항상 세력들에게 끌려다니는 개미들도 뭉치면 강하다!", new ArrayList<String>(Arrays.asList("국내주식","해외주식","세력")));
+
+		return new JSONArray().put(standard.toJsonObject()).put(standard.toJsonObject()).put(example1.toJsonObject()).put(example2.toJsonObject()).put(example3.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).toString();
 	}
 	
 	@PostMapping("/myClubBanners")
-	public String myClub(HttpServletRequest request) {
+	public String myClub(HttpServletRequest request) throws JSONException, ParseException {
 		//test value
-		JSONArray ja=new JSONArray();
-		JSONObject jo=new JSONObject();
-		JSONObject jo1=new JSONObject();
-		JSONObject jo2=new JSONObject();
-		JSONObject jo3=new JSONObject();
-		try {
-			jo.put("name", "클럽 이름");
-			jo.put("concludedContract", "26");
-			jo.put("totalContract", "180");
-			jo.put("member", "111");
-			jo.put("recentActivities", "1개월");
-			ja.put(jo);
-			ja.put(jo);
-			ja.put(jo);
-			ja.put(jo);
-			ja.put(jo);
-			jo1.put("name", "휴양지 공동 개발");
-			jo1.put("concludedContract", "15");
-			jo1.put("totalContract", "22");
-			jo1.put("member", "411");
-			jo1.put("recentActivities", "2주");
-			ja.put(jo1);			
-			jo2.put("name", "홍대 갤러리");
-			jo2.put("concludedContract", "46");
-			jo2.put("totalContract", "2130");
-			jo2.put("member", "11.5k");
-			jo2.put("recentActivities", "48분");
-			ja.put(jo2);			
-			jo3.put("name", "동학 개미 운동");
-			jo3.put("concludedContract", "71");
-			jo3.put("totalContract", "182");
-			jo3.put("member", "85.4k");
-			jo3.put("recentActivities", "11일");
-			ja.put(jo3);
-			ja.put(jo);
-			ja.put(jo);
-			ja.put(jo);
-			ja.put(jo);
-			ja.put(jo);
-			ja.put(jo);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		
-		//return "";
-		return ja.toString();
+		ClubBanner standard =new ClubBanner("클럽이름", 26, 180, 111, "2022/03/30 16:22:45", "클럽 소개글\n클럽 소개글", new ArrayList<String>(Arrays.asList("품목","품목","품목","품목")));
+		ClubBanner example1=new ClubBanner("휴양지 공동 개발",15,22,411,"2021/03/29 20:11:02","코로나로 저점이지만 앞으로 전망이 밝은 각종 휴양지를 전문으로 투자하실분들 모십니다.", new ArrayList<String>(Arrays.asList("휴양지","부동산","저점")));
+		ClubBanner example2=new ClubBanner("홍대 갤러리",46,2130,11512,"2022/03/30 9:42:16","홍대 작가 유망주들의 작품 지분 투자", new ArrayList<String>(Arrays.asList("그림","조각")));
+		ClubBanner example3=new ClubBanner("동학 개미 운동",71,182,85429,"2022/02/15 20:11:02","소자본으로 항상 세력들에게 끌려다니는 개미들도 뭉치면 강하다!", new ArrayList<String>(Arrays.asList("국내주식","해외주식","세력")));
+
+		return new JSONArray().put(standard.toJsonObject()).put(standard.toJsonObject()).put(example1.toJsonObject()).put(example2.toJsonObject()).put(example3.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).put(standard.toJsonObject()).toString();
 	}
 	
 	@PostMapping("/auth")
--- virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/data/ClubBanner.java
+++ virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/data/ClubBanner.java
@@ -1,46 +1,112 @@
 package com.virtualAsset.webServer.data;
 
-import java.sql.Date;
 import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 
 public class ClubBanner {
 	private static DecimalFormat df=new DecimalFormat("###.#");
-	private static String[] units= {" ","k","M","G","T"};
+	private static String[] units= {"","k","M","G","T"};
+	private SimpleDateFormat strToDate=new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+	
 	private String name;
 	private int concludedContractAmount;
 	private int totalContractAmount;
 	private int memberAmount;
-	Date recentActivities;
-	public ClubBanner(String name, int cca, int tca, int ma, Date ra) {
+	private String introduce;
+	private ArrayList<String> tags;
+	String recentActivities;
+	
+	public ClubBanner(String name, int cca, int tca, int ma, String ra, String intro, ArrayList<String> tags) {
 		this.concludedContractAmount=cca;
 		this.memberAmount=ma;
 		this.name=name;
 		this.recentActivities=ra;
 		this.totalContractAmount=tca;
+		this.introduce=intro;
+		this.tags=new ArrayList<String>(tags);
 	}
+	
 	public String getName() {
 		return name;
 	}
-	public int getConcludedContract() {
+	public int getIntConcludedContract() {
 		return concludedContractAmount;
+	}	
+	public String getStringConcludedContract() {
+		return intToRoundString(concludedContractAmount);
 	}
-	public int getTotalContract() {
+	public int getIntTotalContract() {
 		return totalContractAmount;
 	}
-	public String getMember() {
-		//return memberAmount;
-		//*
+	public String getStringTotalContract() {
+		return intToRoundString(totalContractAmount);
+	}
+	public int getIntMemberAmt() {
+		return memberAmount;
+	}
+	public String getStringMemberAmt() {
+		return intToRoundString(memberAmount);
+	}
+	public String getRecentActivitiesDate() {
+		return recentActivities;
+	}
+	public String getIntrduce() {
+		return introduce;
+	}
+	public ArrayList<String> getTags(){
+		return tags;
+	}
+	public String getRecentActivitiesDiff() throws ParseException {
+		Date d=strToDate.parse(recentActivities);
+		int diff=(int) ((System.currentTimeMillis()-d.getTime())/1000);
+		if(diff>60) {
+			diff/=60;
+			if(diff>60) {
+				diff/=60;
+				if(diff>24) {
+					diff/=24;
+					if(diff>30) {
+						diff/=30;
+						if(diff>12) {
+							diff/=12;
+							return diff+"년";
+						}
+						return diff+"개월";
+					}
+					return diff+"일";
+				}
+				return diff+"시간";
+			}
+			return diff+"분";
+		}
+		return diff+"초";
+	}
+	public JSONObject toJsonObject() throws JSONException, ParseException {
+		JSONObject temp=new JSONObject();
+		temp.put("name", name);
+		temp.put("concludedContract", getStringConcludedContract());
+		temp.put("totalContract", getStringTotalContract());
+		temp.put("memberAmount", getStringMemberAmt());
+		temp.put("recentActivities", getRecentActivitiesDiff());
+		temp.put("introduce", introduce);
+		temp.put("tags", new JSONArray(tags));
+		return temp;
+	}
+	
+	public static String intToRoundString(int val) {
 		int t=1, index=0;
 		 
-		while(memberAmount<t*1000) {
+		while(val>t*1000) {
 			index++;
 			t*=1000;
 		}
-		return df.format(memberAmount/t)+units[index];
-	//*/
+		return df.format((double)(val)/t)+units[index];
 	}
-	public Date getRecentActivities() {
-		return recentActivities;
-	}
-	
 }
