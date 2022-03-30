ee496b0b85720d49fe4ed73759915408949b348d
tjdcks098 <44407212+tjdcks098@users.noreply.github.com>
2022-03-30 11:35:43 +0900

    non-js module files deprecated 문제 해결
    frontend의 setupProxy에 setTestIp를 import하면서 발생.
    proxy ip는 수동으로 바꿔야할듯

--- virtualAssetWebServer/frontend/src/setupProxy.js
+++ virtualAssetWebServer/frontend/src/setupProxy.js
@@ -1,11 +1,10 @@
-import TEST_IP from './scripts/setTestIp';
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api/*',
-    createProxyMiddleware({
-      target: 'http://'+TEST_IP+':8080',
+    createProxyMiddleware('/api/*',{
+      target: 'http://220.70.59.102:8080',
       changeOrigin: true,
     })
   );
