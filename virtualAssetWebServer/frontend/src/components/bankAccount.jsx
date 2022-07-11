c5ba2e2de2194439e4d2b3f5aba9946958713812
yevinKwon <kimpika8541@naver.com>
2022-07-11 09:16:36 +0900

    계좌 연결 페이지 업데이트
    - 연결된 계좌 없는 경우 안내문구 표시

--- virtualAssetWebServer/frontend/src/components/bankAccount.jsx
+++ virtualAssetWebServer/frontend/src/components/bankAccount.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import customAxios from "../scripts/customAxios";
 import Logoff from "./logoff";
+import "./home.css";
 import LoadingSpinner from "./loadingSpinner";
+
+import No_contents_blue from "../Icons/No_contents_blue.png";
 import './bankAccount.css';
 
 export default function BankAccount(props) {
@@ -27,25 +30,38 @@ export default function BankAccount(props) {
 
 		  if (props.auth !== undefined && props.auth.code === 100) {
 			if(isLoaded){
-			return <BankAccountLogon
-			bankAccountPage={props.bankAccountPage}
-				className={props.className}
-				bankName={props.bankName}
-				accountNumber={props.accountNumber}
-				bankAccounts={bankAccountDisplay}
-				auth={props.auth}
-				hideBankAccountPage={props.hideBankAccountPage}
-			/>;
-			}
+				return <BankAccountLogon
+				bankAccountPage={props.bankAccountPage}
+					className={props.className}
+					bankName={props.bankName}
+					accountNumber={props.accountNumber}
+					bankAccounts={bankAccountDisplay}
+					auth={props.auth}
+					hideBankAccountPage={props.hideBankAccountPage}
+				/>;
+				}
 			return <div className={props.className}><LoadingSpinner></LoadingSpinner></div>
-	   }
-	   else {
-			return <div></div>
-			// return <Logoff className={props.className} showSigninPage={props.showSigninPage} showSignupPage={props.showSignupPage} />;
-	   }
-	}
+		}	
 
 	function BankAccountLogon(props) {
+	//연결된 계좌 없을때
+	if (props.bankAccounts === null) {
+		return (
+			<div className={'bankAccountBackground'+(props.bankAccountPage==='hide'?'bankAccountHide':'')} style={{zIndex:10002}}>
+			<nav>
+				<ul className="nav-container">
+					<li className="nav-item" ><span onClick={props.hideBankAccountPage}><i className="fa fa-arrow-left"></i></span></li>
+					<li className="nav-item"><p>결제</p></li>
+				</ul>
+			</nav>
+			<img id="no_contents" src={No_contents_blue} alt="img_no_contents" />
+			<p className='no_accounts'>안녕하세요, {props.auth.name}님!<br />
+			아직 연결된 계좌가 없어요.</p>
+			<p className='no_accounts' >지금 계좌를 연결하고 투자를 시작해보세요.<br /></p>
+			<button className='addAccountBtn' onClick={props.hideBankAccountPage}>계좌 등록</button>
+			</div>
+		);
+	}
 	return (
 		<div className={'bankAccountBackground'+(props.bankAccountPage==='hide'?'bankAccountHide':'')} style={{zIndex:10002}}>
 			<nav>
@@ -55,18 +71,20 @@ export default function BankAccount(props) {
 				</ul>
 			</nav>
 			<div className={props.bankAccounts}>
-				<div className="bankAccountListInfo">등록된 계좌목록</div>
-					<div className="myAccountList">
-						{props.bankAccounts.map((accounts, i) => (
+			<div className={props.className}>
+	 			<div className="bankAccountListInfo">등록된 계좌목록</div>
+	 				<div className="myAccountList">
+	 					{props.bankAccounts.map((accounts, i) => (
 							<div className="accountInfoBanner" key={accounts.bankName + i}>
 								<p className="bankName">{accounts.bankName}</p>
 								<p className="accountNumber">{accounts.accountNumber}</p>
 							</div>
 						))}
 					</div>
+					
+				</div>
 			</div>
-			<button className='addAccountBtn' onClick={props.hideBankAccountPage}>계좌 등록</button>
 		</div>
-	)
+	);}
 }
 
