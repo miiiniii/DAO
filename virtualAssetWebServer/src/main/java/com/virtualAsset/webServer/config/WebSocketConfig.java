a2834b6e223d74515eee6a89b9e89b788ef4d840
tjdcks098 <44407212+tjdcks098@users.noreply.github.com>
2022-07-13 17:05:17 +0900

    임시커밋

--- virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/config/WebSocketConfig.java
+++ virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/my-chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/api/my-chat").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
