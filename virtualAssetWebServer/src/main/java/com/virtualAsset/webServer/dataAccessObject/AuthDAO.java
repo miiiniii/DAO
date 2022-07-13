59a825034c46e9a653389100583a503048be5696
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-07-13 16:38:26 +0900

    [fix] @Mapper 어노테이션 추가

--- virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/dataAccessObject/AuthDAO.java
+++ virtualAssetWebServer/src/main/java/com/virtualAsset/webServer/dataAccessObject/AuthDAO.java
@@ -1,7 +1,9 @@
 package com.virtualAsset.webServer.dataAccessObject;
 
 import com.virtualAsset.webServer.entity.AuthEntity;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AuthDAO {
 	
 	public String getPassword(String id);
