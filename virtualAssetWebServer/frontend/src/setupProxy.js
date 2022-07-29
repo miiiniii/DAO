c9ed25c0d09b5f6d1d70270831b827e41d00f6f9
Seongjoo Woo <violicsunny@naver.com>
2022-07-29 15:20:33 +0900

    채널 상태 표시

--- virtualAssetWebServer/frontend/src/setupProxy.js
+++ virtualAssetWebServer/frontend/src/setupProxy.js
@@ -4,12 +4,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware('/api/**',{
-      /*안성찬 로컬*/target: 'http://211.193.12.31:8080',
+      //*안성찬 로컬*/target: 'http://211.193.12.31:8080',
 
       //*안성찬 회사*/target: 'http://192.168.30.14:8080',
 
-      /*지상은 로컬*/target:'http://172.30.1.35:8080',
+      //*지상은 로컬*/target:'http://172.30.1.35:8080',
 
+      /*우성주 z동*/target:'http://192.168.0.6:8080',
+      //*우성주 t동*/target:'http://192.168.0.148:8080',
 
       //*지상은 로컬*/target:'http://172.30.1.34:3000',
 
